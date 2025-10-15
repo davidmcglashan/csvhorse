04c45c9a418bcb8f9192999134f1d7e699d0393b
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2025-10-15 19:55:57 +0100

    Chunky rewrite to do funcs() in a nice stateful way

--- js/recipe.js
+++ js/recipe.js
@@ -3,7 +3,7 @@ const recipe = {
 	// - Major releases see significant change to the feature set e.g. multiple minors.
 	// - Minor changes when at least one command is added, removed or changed, or a UI feature is added.
 	// - Point releases for bug fixes, UI modifications, meta and build changes.
-	version: "v0.0.7",
+	version: "v0.1.0",
 
 	/*
 	* Executes the currently entered recipe.
@@ -28,19 +28,18 @@ const recipe = {
 		// Get the recipe text, split by newlines so we can parse each one in turn. 
 		let recipeLines = document.getElementById('rec').value.split('\n')
 		
-		lineLoop: for ( let line of recipeLines ) {
+		for ( let line of recipeLines ) {
 			// Ignore comments and blank lines
 			if ( line.length === 0 || line.startsWith('//') ) {
 				continue
 			}
 
-			// If the line is a directive then let it update the config
+			// If the line is a directive then let it update the config.
 			let tokens = line.split( ' ' )
-			for ( let key in directives ) {
-				if ( tokens[0] === key ) {
-					directives[key]( tokens, config )
-					continue lineLoop
-				}
+			let directive = directives[tokens[0]]
+			if ( directive ) {
+				directive( tokens, config )
+				continue
 			}
 
 			// Treat this line as a column definition.
@@ -64,19 +63,36 @@ const recipe = {
 			numberOfCols = config.columns.count
 		}
 
-		// At this point we have the number of rows and columns, and all the definitions so we can start generating content
-		// into the model.
+		// At this point we have the number of rows and columns, and all the definitions
+		// so we can start generating content into the model.
 		for ( let c=0; c<numberOfCols; c+=1 ) {
 			let defn = config.columns.defns[c]
 
-			// Process the definition
-			let spec = recipe.processDefinition( defn )
+			// Process the definition to get an array of tokens.
+			let colObj = recipe.getColumnObject( defn )
 
 			// Now use it to generate content for each row!
 			for ( let r=0; r<config.rows; r+=1 ) {
 				let column = {}
 				model.rows[r].columns[c] = column
-				column.content = spec.next( r+1 )
+				
+				column.content = ''
+				for ( let token of colObj.tokens ) {
+					// If the token is a string we can simply concat
+					if ( typeof token === 'string' ) {
+						column.content += token
+					} else {
+						// Patterns can simply be written
+						if ( token.pattern ) {							
+							column.content += recipe.getStringFromPattern( recipe.variables[token.pattern] )
+						} 
+						
+						// Functions require some execution
+						else if ( token.function ) {
+							column.content += funcs[token.function]( token )
+						}
+					}
+				}
 			}
 		}
 
@@ -109,83 +125,110 @@ const recipe = {
 	},
 
 	/**
-	 * Turns a string definition into a spec which can generate content in the CSV.
+	 * Turns a string definition into an array of tokens which in turn will generate the
+	 * content of the CSV. Tokens are either strings or objects which contain state that can
+	 * be evaluated into strings.
 	 */
-	processDefinition: ( defn ) => {
-		// A null definition results in an empty string.
-		if ( !defn ) {
-			return{ next: () => { return '' } }
+	getColumnObject: ( defn ) => {
+		// A null definition results in a single empty string in the CSV.
+		if ( !defn || defn.length === 0 ) {
+			return {tokens:['']}
 		}
 
-		let spec = null
-		let tokens = defn.split( ' ' )
+		// Definitions can come in two halves separated by a | e.g. "template|config"
+		let arr = defn.split('|')
+		let template = arr[0]
+		let column = {}
+		column.tokens = []
 
-		// Check for variables
-		if ( tokens[0].startsWith( '$' ) ) {
-			let value = recipe.variables[tokens[0].substr(1)]
-			if ( value ) {
-				spec = { compose: ( row ) => { return recipe.replacePatternsInString( value, row ) } }
-			}
+		// Replace any known variables in the template.
+		for ( let key in recipe.variables ) {
+			template = template.replaceAll( '$'+key, recipe.variables[key] )
 		}
 
-		// Check for leading "
-		if ( tokens[0].startsWith( '"' ) ) {
-			let value = defn.substr(1)
-			let closing = value.indexOf('"')
-
-			// If the closing quote is at the very end then the definition string is what we'll use
-			if ( closing === -1 || closing === value.length-1 ) {
-				return{ next: () => { return defn } }
-			}
-
-			spec = { compose: () => { return value.substr(0,closing) } }
-		}
-
-		// Check for patterns
-		if ( tokens[0].startsWith( '%' ) ) {
-			let pattern = recipe.variables[tokens[0].substr(1)]
-			if ( pattern ) {
-				spec = { compose: () => { return recipe.getStringFromPattern( pattern ) } }
-			}
-		}
-
-		// Check for funcs
-		if ( tokens[0].startsWith( '@' ) ) {
-			let func = tokens[0].substr(1)
-			let brace = func.indexOf('(')
-
-			// If there's no brace then there's no function. Use the defn string instead.
-			if ( brace === -1 ) {
-				return{ next: () => { return defn } }
+		// Search for patterns in the template.
+		let i = 0
+		searchLoop: while ( i < template.length ) {
+			// A % char means a pattern might follow ...
+			if ( template[i] === '%' ) {
+				for ( let key in recipe.variables ) {
+					let index = template.indexOf( '%'+key )
+					// Does it match any of the variables?
+					if ( index === i ) {
+						// Tokenise the search string and the %pattern 
+						column.tokens.push( template.substr(0,index) )
+						column.tokens.push( { pattern:key } )
+						
+						// Chop the tokenised bits from the template and resume the search
+						template = template.substring( index+key.length+1 )
+						i = 0
+						continue searchLoop
+					}
+				}
 			}
 
-			spec = { compose: ( row ) => { return funcs[func.substr(0,brace)]( defn.split(' ')[0], row ) } }
+			// An @ means a function call might follow ...
+			if ( template[i] === '@' ) {
+				for ( let key in funcs ) {
+					let index = template.indexOf( '@'+key+'(' )
+					// Does it match any of the functions, including the ) at the end?
+					if ( index === i ) {
+						let end = template.indexOf( ')', index )
+						if ( end !== -1 ) {
+							// Tokenise the search string.
+							column.tokens.push( template.substr(0,index) )
+
+							let vars = template.substring( index+key.length+2,end)
+							column.tokens.push( { 
+								function: key,
+								vars: vars
+							} )
+							
+							// Chop the tokenised bits from the template and resume the search
+							template = template.substring( end+1 )
+							i = 0
+							continue searchLoop
+						}
+					}
+				}				
+			}
+			i += 1
 		}
 
-		// If none of the above worked, create a simple next that returns the original definition string.
-		if ( spec === null ) {
-			return{ next: () => { return defn } }
-		}
+		// Include everything that's left in the template (could be everything!)
+		column.tokens.push( template )
 
-		// The default next() function calls compose(), unless modified by what follows ...
-		spec.next = function( row ) { return this.compose( row ) }
+		// Configs are easy to configure
+		//if ( arr.length === 2 ) {
+		//	recipe.processConfig( arr[1], spec )
+		//}
+
+		// Everything failed!
+		return column
+	},
+
+	/**
+	 * Parses the config part of a defn string and configures the spec to act accordingly.
+	 */
+	processConfig: ( config, spec ) => {
+		// Configs are space-separated directives we parse one-by-one
+		let tokens = config.split(' ')
 
 		// Is there an empty keyword?
 		for ( let t=0; t<tokens.length; t+=1 ) {
 			if ( tokens[t] === 'empty' ) {
 				let pctage = parseInt(tokens[t+1])
-				spec.next = function( row ) {
+				spec.nextValue = function() {
 					if ( random.get( 0, 100 ) < pctage ) {
 						return ''
 					} else {
-						return this.compose( row )
+						return this.compose()
 					}
 				}
 				break
 			}
 		}
 
-		return spec
 	},
 
 	/**
@@ -204,32 +247,6 @@ const recipe = {
 		}
 
 		recipe.variables = dict
-	},
-
-	/**
-	 * Checks the ins string for instances of any %pattern in vars and swaps in its
-	 * newly-generated value.
-	 */
-	replacePatternsInString: ( ins, row ) => {
-		// Patterns are variables prefixed with %. We want to generate a new result every time
-		// we do a substitution.
-		let index = 0
-		for ( const [ key, value ] of Object.entries( recipe.variables ) ) {
-			index = ins.indexOf( '%'+key )
-			while ( index !== -1 ) {
-				ins = ins.replace( '%'+key, recipe.getStringFromPattern( value ) )
-				index = ins.indexOf( '%'+key )
-			}
-		}
-
-		// Let the funcs do their func'ing.
-		ins = funcs.rng( ins, row )
-		ins = funcs.lorem( ins, row )
-		ins = funcs.list( ins, row )
-		ins = funcs.count( ins, row )
-
-		// Send back the finished string
-		return ins
 	},
 
 	/**
@@ -336,142 +353,169 @@ const directives = {
 
 const funcs = {
 	/**
-	 * Implements a count by adding the passed-in row number to the start number defined in the func.
+	 * Implements a counter using the function's saved state to increment. The next value of the
+	 * function is returned for inclusion in the CSV.
+	 * @count() starts from 1 and increments by 1
+	 * @count(23) starts from 23 and increments by 1
+	 * @count(5 step 5) starts from 5 and increments by 5
+	 * @count(step 10) starts from 1 and increments by 10
+	 * @count(skip 50) starts from 1 and increments by 1 with a 50% probability of a value being skipped.
 	 */
-	count: ( ins, row=1 ) => {
-		let start = ins.indexOf( '@count(' )
-		while ( start !== -1 ) {
-			// Parses the content, expecting a number. 
-			let end = ins.indexOf( ')', start )
-			if ( end === -1 ) {
-				return ins
+	count: ( func ) => {
+		// Set up the function first.
+		if ( !func.current ) {
+			// These are the default values. We'll use these if something goes wrong.
+			func.current = 1
+			func.step = 1
+			func.skip = 0
+
+			if ( func.vars ) {
+				let vars = func.vars.split( ' ' )
+				let i = 0
+
+				// The first parameter being a number can only be a starting index.
+				let start = parseInt( vars[i] )
+				if ( !isNaN( start ) ) {
+					func.current = start
+					i += 1
+				}
+
+				while ( i < vars.length ) {
+					// Next parameter must be a recognised word.
+					if ( vars[i] === 'step' ) {
+						if ( vars[i+1] ) {
+							let step = parseInt( vars[i+1] )
+							if ( !isNaN( step ) ) {
+								func.step = step
+							}	
+						}
+					}
+					// Next parameter must be a recognised word.
+					else if ( vars[i] === 'skip' ) {
+						if ( vars[i+1] ) {
+							let skip = parseInt( vars[i+1] )
+							if ( !isNaN( skip ) ) {
+								func.skip = skip
+							}	
+						}
+					}
+
+					i += 1
+				}
 			}
 
-			let val = ins.substr(start+7,end-start-7)
-			let num = parseInt(val)
-
-			// Any problems result in the original string being returned untouched.
-			if ( !isNaN(num) ) {
-				ins = ins.substr( 0,start ) + (num+row-1) + ins.substr( end+1 )
-			} else {
-				ins = ins.substr( 0,start ) + (row) + ins.substr( end+1 )
-			}
-
-			start = ins.indexOf( '@count(' )
-		}	
-
-		return ins	
-	},
-	
-	/**
-	 * Generates a random number based on the input of the line: @rng(1,10) => picks a number between 1 and 10
-	 */
-	rng: ( ins ) => {
-		// @rng(min,max) generates random numbers
-		let start = ins.indexOf( '@rng(' )
-		while ( start !== -1 ) {
-			// Parses the content, expecting two comma-separated numbers. 
-			let end = ins.indexOf( ')', start )
-			if ( end === -1 ) {
-				return ins
-			}
-
-			let vals = ins.substr(start+5,end-start-5).split(',')
-			if ( vals.length !== 2 ) {
-				return ins
-			}
-
-			// Any problems result in the original string being returned untouched.
-			let result = random.get( parseInt(vals[0]), parseInt(vals[1]) )
-			if ( !isNaN(result) ) {
-				ins = ins.substr( 0,start ) + result + ins.substr( end+1 )
-			} else {
-				return ins
-			}
-
-			start = ins.indexOf( '@rng(' )
+			return func.current
 		}
 
-		return ins
+		// Non-zero skips mean that sometimes we skip a value when counting.
+		if ( func.skip > 0 ) {
+			while ( random.get(0,100) < func.skip ) {
+				func.current += func.step
+			}
+		}
+
+		// Increment the counter and return the new value.
+		func.current += func.step
+		return func.current
 	},
 	
 	/**
-	 * @lorem(num) inserts N words of lorem ipsum ... @lorem(3) => three words of lipsum
+	 * Implements a random number generator. The genreated value is returned for inclusion in the CSV.
+	 * @rng() generates a number between 1 and 100
+	 * @rng(10) generates a number between 1 and 10
+	 * @rng(25,75) generates a number between 25 and 75
 	 */
-	lorem: ( ins ) => {
-		let start = ins.indexOf( '@lorem(' )
-		while ( start !== -1 ) {
-			// Parses the content, expecting a number. 
-			let end = ins.indexOf( ')', start )
-			if ( end === -1 ) {
-				return ins
+	rng: ( func ) => {
+		// Set the function up if it hasn't yet been.
+		if ( !func.max ) {
+			func.max = 100
+			func.min = 1
+
+			// We may have one or two comma-separated numbers in the variables.
+			if ( func.vars ) {
+				let vars = func.vars.split( ',' )
+
+				// One parameter means we're changing the max.
+				if ( vars.length === 1 ) {
+					let max = parseInt( vars[0] )
+					if ( !isNaN( max ) ) {
+						func.max = max
+					}
+				}
+
+				// Two parameters means changing both min and max
+				if ( vars.length > 1 ) {
+					let min = parseInt( vars[0] )
+					if ( !isNaN( min ) ) {
+						func.min = min
+					}
+					let max = parseInt( vars[1] )
+					if ( !isNaN( max ) ) {
+						func.max = max
+					}
+				}
 			}
+		}
 
-			let val = ins.substr(start+7,end-start-7)
-
-			// An 'r' means random words from the sequence, 'c' means capitalise the first word (like a sentence)
-			let rnd = val.indexOf( 'r' ) != -1
-			let cap = val.indexOf( 'c' ) != -1
-			let num = parseInt(val)
-
-			// Any problems result in the original string being returned untouched.
-			if ( isNaN(num) ) {
-				return ins
-			} else {
-				ins = ins.substr( 0,start ) + funcs.getLipsum(num,rnd,cap) + ins.substr( end+1 )
-			}
-
-			start = ins.indexOf( '@lorem(' )
-		}	
-
-		return ins	
+		return random.get( func.min, func.max )
 	},
-
+	
 	/**
-	 * Generates num words of lower-case Lorem Ipsum text. rnd will shuffle the words. cap will capitalise the first word.
+	 * Generates Lorem Ipsum passages for inclusion in the CSV. Returned strings are capitalised.
+	 * @lorem() returns 5 words of lorem ipsum
+	 * @lorem(10) returns 10 words of lorem ipsum
+	 * @lorem(7 random) returns 10 _random_ words of lorem ipsum
 	 */
-	getLipsum: ( num, rnd, cap ) => {
+	lorem: ( func ) => {
+		// Set the function up if it hasn't yet been.
+		if ( !func.numberOfWords ) {
+			func.numberOfWords = 5
+			func.random = false
+			func.capitalise = true
+
+			if ( func.vars ) {
+				// An 'r' means random words from the sequence, 'c' means capitalise the first word (like a sentence)
+				func.random = func.vars.indexOf( 'random' ) != -1
+				//func.capitalise = func.vars.indexOf( 'c' ) != -1
+				func.numberOfWords = Math.min( 35, parseInt(func.vars) )
+			}
+		}
+
 		const lipsum = 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'.split(' ')
 		let ret = ''
-		for ( let i=0; i<Math.min(35,num); i++ ) {
+
+		for ( let i=0; i<func.numberOfWords; i++ ) {
 			if ( i > 0 ) {
 				ret += ' '
 			}
-			ret += rnd ? lipsum[random.get(0,35)] : lipsum[i]
+			ret += func.random ? lipsum[random.get(0,35)] : lipsum[i]
 		}
-
-		if ( cap ) {
-			ret = ret[0].toUpperCase() + ret.substr( 1 )
+	
+		if ( func.capitalise ) {
+			ret = ret[0].toUpperCase() + ret.substring( 1 )
 		}
 		return ret
 	},
 
 	/**
-	 * Chooses a random entry from a variable which was defined as a comma-separated list ...
-	 * @list(foo) => A random entry from the list in $foo variable
+	 * Chooses a random entry from a variable's value which was defined as a comma-separated list for
+	 * inclusion in the CSV.
+	 * @oneof(foo) => A random entry from the list in $foo variable
 	 */
-	list: ( ins ) => {
-		let start = ins.indexOf( '@list(' )
-		while ( start !== -1 ) {
-			// Parses the content, expecting a string. 
-			let end = ins.indexOf( ')', start )
-			if ( end === -1 ) {
-				return ins
+	oneof: ( func ) => {
+		if ( !func.entries ) {
+			// Use an empty list if we can't find the specified list.
+			func.entries = ['']
+
+			if ( func.vars ) {
+				let list = recipe.variables[func.vars.trim()]
+				if ( list ) {
+					func.entries = list.split( ',' )
+				}
 			}
-			let list = recipe.variables[ins.substr(start+6,end-start-6)]
+		}
 
-			// Any problems result in the original string being returned untouched.
-			if ( !list ) {
-				return ins
-			} else {
-				entries = list.split(',')
-				ins = ins.substr( 0,start ) + entries[random.get(0,entries.length-1)]+ ins.substr( end+1 )
-			}
-
-			start = ins.indexOf( '@list(' )
-		}	
-
-		return ins	
+		return func.entries[ random.get( 0, func.entries.length-1 ) ]
 	}
 };
 
